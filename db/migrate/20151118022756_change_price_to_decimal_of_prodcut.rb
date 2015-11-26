ba7566b0b2033558ba70c703407739d4d4e6398e
elaine <hbotelloelaine@gmail.com>
2015-11-25 18:16:22 -0800

    fix migrations

--- db/migrate/20151118022756_change_price_to_decimal_of_prodcut.rb
+++
@@ -1,5 +0,0 @@
-class ChangePriceToDecimalOfProdcut < ActiveRecord::Migration
-  def change
-    change_column :products, :price, 'decimal USING(trim(price)::decimal, presision: 6, scale: 2)'
-  end
-end
